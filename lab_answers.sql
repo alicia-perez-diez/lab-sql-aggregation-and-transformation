0d0dcb43457ccc4525352c57be9e328df6ae6102
alicia-perez-diez <perezdiezalicia9@gmail.com>
2024-05-01 22:55:19 +0200

    V_2

--- lab_answers.sql
+++ lab_answers.sql
@@ -1,32 +1,16 @@
 -- Determina la película más larga y más corta y llama a los nuevos valores max_duration y min_duration
-SELECT
-	title, 
-	length AS max_duration
-FROM sakila.film
-ORDER BY length desc
-LIMIT 1;
 
-SELECT
-	title, 
-	length AS min_duration
-FROM sakila.film
-ORDER BY length asc
-LIMIT 1;
-
-	-- la de máxima duración es CHICAGO NORTH con 185 minutos
-    -- la de mínima duración es ALIEN CENTER con 46 minutos
-    
--- Expresa la duración media de las películas en horas y minutos sin decimales
-SELECT 
-	CONCAT(
-		FLOOR(AVG(length)/60),
-        ' hours ',
-        FLOOR(AVG(length) MOD 60),
-        ' minutes '
-	) AS avg_length
+SELECT MAX(length) AS max_duration, MIN(length) AS min_duration
 FROM sakila.film;
 
-	-- 1 hora y 55 minutos
+	-- la de máxima duración es 185 minutos
+    -- la de mínima duración es 46 minutos
+    
+-- Expresa la duración media de las películas en horas y minutos sin decimales
+SELECT (ROUND(AVG(length)/60)) AS hours, (ROUND(AVG(length) MOD 60)) AS minutes
+FROM sakila.film;
+
+	-- 2 horas y 55 minutos
 
 -- Calcula el número de días que la compañía ha operado
 
@@ -36,12 +20,62 @@ FROM sakila.rental;
 	-- 266 días
     
 -- Extrae la información del rental y 2 columnas adicionales que muestren el mes y el día de la semana del rental. Devuelve 20 filas.
-SELECT
-    DATE_FORMAT(CONVERT(rental_date, DATE), '%M' AS 'month',
+SELECT *, DATE_FORMAT(rental_date, '%M') AS month, DATE_FORMAT(rental_date, '%W') AS day    
 FROM sakila.rental;
-    
-    
-    
+
+-- Extrae la información del rental y añade una columna más que se llame DAY_TYPE con valores 'weekend' o 'workday'
+SELECT *,
+CASE 
+	WHEN DATE_FORMAT(rental_date, '%W') IN ('Saturday', 'Sunday')
+	THEN 'weekend'
+	ELSE 'workday' 
+END AS day_type
+FROM sakila.rental;
+
+-- Extrae los títulos y duración y añade 'Not available' si hay valores nulos
+SELECT title, IFNULL(rental_duration, "Not available") AS rental_duration
+FROM sakila.film
+ORDER BY title ASC;
+
+-- Extrae el nombre y apellido de los clientes y los 3 primeros caracteres de su email ordenado por apellido en ascendente
+SELECT CONCAT(first_name, " ", last_name) AS full_name, SUBSTRING(email, 1, 3) AS email_prefix
+FROM sakila.customer
+ORDER BY last_name ASC;
+
+-- 4.
+SELECT CONCAT(first_name, ' ', last_name) as full_name, SUBSTRING(email, 1, 3) as email_prefix
+FROM customer
+ORDER BY last_name ASC;
+
+-- 1. número total de películas
+SELECT COUNT(*) AS 'num_films' FROM film;
+
+-- 1.2. número de películas para cada rating
+SELECT rating, COUNT(*) AS 'num_films' FROM film
+GROUP BY rating;
+
+-- 1.3 número de películas para cada rating ordenándolo
+SELECT rating, COUNT(*) AS 'num_films' FROM film
+GROUP BY rating
+ORDER BY num_films DESC;
+
+-- 2. La duración media de cada renting
+SELECT rating, ROUND(AVG(length),2) AS 'avg_length' FROM film
+GROUP BY rating
+ORDER BY avg_length DESC;
+
+-- 2.2 qué ratings tienen una duración superior a las dos horas
+SELECT rating, ROUND(AVG(length),2) AS avg_duration FROM film
+GROUP BY rating
+HAVING avg_duration > 120
+ORDER BY avg_duration DESC;
+
+-- 3. 
+SELECT last_name FROM actor
+GROUP BY last_name
+HAVING COUNT(*) = 1;
+
+
 
 
 
